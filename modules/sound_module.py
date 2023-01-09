70e90948caf54a3312a1a7ed76f55841b2042e7e
nkocodes <82133365+nkocodes@users.noreply.github.com>
2023-01-08 19:27:40 -0800

    Update sound_module.py

--- modules/sound_module.py
+++ modules/sound_module.py
@@ -18,20 +18,20 @@ mixer.init()
 def play_music(mp3):
     song = f"{start}{directory}{mp3}"
     mixer.music.load(song)
-    mixer.music.set_volume(.5)
+    if sound_cut:
+        mixer.music.set_volume(0)
+    else:
+        mixer.music.set_volume(.5)
     mixer.music.play()
-
+    
 #Plays main song    
 def music():
     time.sleep(.6)
     play_music("pokemonthemestart.mp3")
     time.sleep(47.5)
     while True:
-        if sound_cut:
-            mixer.music.set_volume(0)
-        else:
-            play_music("pokemonthemeloop.mp3")
-            time.sleep(43.1)
+        play_music("pokemonthemeloop.mp3")
+        time.sleep(43.1)
 
 #Plays sounds           
 def error_sound():
@@ -42,3 +42,12 @@ def confirmed_sound():
     
 def limit_sound():
     playsound(f"{start}{directory}pokemonlimit.mp3")
+
+#Changes Audio Variable to mute the upon loading song
+def mute_on():
+    global sound_cut
+    sound_cut = True
+    
+def mute_off():
+    global sound_cut
+    sound_cut = False
