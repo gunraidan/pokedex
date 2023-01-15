0bcec1014d9378cce660a5bcfcadab12bca1aef1
nkocodes <82133365+nkocodes@users.noreply.github.com>
2023-01-14 22:38:42 -0800

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,2 +1,2 @@
-#All the modules in the folder
+#All the modules in the folder4
 __all__ = ["ability_module","attack_module","height_weight_module","misc_module","sound_module", "text_updates", "type_module"]
