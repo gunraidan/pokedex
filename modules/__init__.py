8e829dd543e941dd24a3a964823f974d9cd6437e
nkocodes <82133365+nkocodes@users.noreply.github.com>
2023-01-14 22:38:55 -0800

    Modules and assets

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,2 +1,2 @@
-#All the modules in the folder4
+#All the modules in the folder
 __all__ = ["ability_module","attack_module","height_weight_module","misc_module","sound_module", "text_updates", "type_module"]
